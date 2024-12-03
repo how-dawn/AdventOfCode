2d6cc1ce61e47cfdacbaf6588731ef5c22069f17
Haodong Zhao <haodong.zhao.dev@outlook.com>
2024-12-04 00:40:23 +0100

    2024 Day3 P2.

--- 2024/day3/main.cpp
+++ 2024/day3/main.cpp
@@ -38,7 +38,7 @@ std::vector<std::string> preprocessor(const std::string &input,
   auto end = std::sregex_iterator();
 
   for (auto it = begin; it != end; ++it) {
-    // std::cout << it->str() << std::endl;
+    std::cout << "preprocessor match: " << it->str() << std::endl;
     result.push_back(it->str());
   }
 
@@ -74,17 +74,44 @@ long long solverP1(const std::string &input) {
   return result;
 }
 
+long long solverP2(const std::string &input) {
+  long long result = 0LL;
+
+  std::string pattern = R"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))";
+  const auto ops = preprocessor(input, pattern);
+
+  bool enable = true;
+  std::string pattern2 = R"(mul\((\d{1,3}),(\d{1,3})\))";
+  for (const auto op : ops) {
+    if (op == "don't()") {
+      enable = false;
+      continue;
+    }
+
+    if (op == "do()") {
+      enable = true;
+      continue;
+    }
+
+    if (enable) {
+      result += calculate(op, pattern2);
+    }
+  }
+
+  return result;
+}
+
 int main(int argc, char *argv[]) {
   std::string filePath = argv[1];
 
   auto input = readInput(filePath);
-  //   std::cout << "Input: " << input << std::endl;
+  std::cout << "Input: " << input << std::endl;
 
   long long resultP1 = solverP1(input);
   std::cout << "P1 result: " << resultP1 << std::endl;
 
-  //   int resultP2 = solverP2(input);
-  //   std::cout << "P2 result: " << resultP2 << std::endl;
+  long long resultP2 = solverP2(input);
+  std::cout << "P2 result: " << resultP2 << std::endl;
 
   return 0;
 }
