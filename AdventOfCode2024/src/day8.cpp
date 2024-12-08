b43702f5ba355741575ab99ff78e69c9668af3d9
Haodong Zhao <haodong.zhao.dev@outlook.com>
2024-12-08 15:53:15 +0100

    2024 day 8 P2.

--- AdventOfCode2024/src/day8.cpp
+++ AdventOfCode2024/src/day8.cpp
@@ -55,7 +55,8 @@ bool isInMap(const std::vector<std::string> &inputData, const Coord &coord) {
     return x >= 0 && x < xSize && y >= 0 && y < ySize;
 }
 
-std::set<Coord> findAntiNodes(const std::vector<std::string> &inputData, const char c, const std::set<Coord> &coords) {
+std::set<Coord>
+findAntiNodesP1(const std::vector<std::string> &inputData, const char c, const std::set<Coord> &coords) {
     std::set<Coord> result;
 
     // pair the antennas
@@ -87,30 +88,22 @@ std::set<Coord> findAntiNodes(const std::vector<std::string> &inputData, const c
                 bY += dY2;
             }
 
-//            std::cout << "Chain: " << std::endl;
-//            for (const auto &[a, b]: connectionSet) {
-//                std::cout << "(" << a << ", " << b << ") ";
-//            }
-//            std::cout << std::endl;
             const auto &[firstAntennaX, firstAntennaY] = *connectionSet.begin();
             const auto &[lastAntennaX, lastAntennaY] = *std::prev(connectionSet.end(), 1);
 
             const int newAX = firstAntennaX + dX1, newAY = firstAntennaY + dY1;
             if (isInMap(inputData, {newAX, newAY}) && inputData[newAX][newAY] != c) {
-//                std::cout << "1. find antinode for " << c << ": " << newAX << ", " << newAY << std::endl;
                 result.insert({newAX, newAY});
             }
 
             const int newBX = lastAntennaX + dX2, newBY = lastAntennaY + dY2;
             if (isInMap(inputData, {newBX, newBY}) && inputData[newBX][newBY] != c) {
-//                std::cout << "2. find antinode for " << c << ": " << newBX << ", " << newBY << std::endl;
                 result.insert({newBX, newBY});
             }
         }
     }
 
     return result;
-
 }
 
 
@@ -120,10 +113,62 @@ int solverP1(const std::vector<std::string> &inputData) {
     const auto inputMap = preprocessor(inputData);
 
     for (const auto &[c, cSet]: inputMap) {
-        const auto &antiNodes = findAntiNodes(inputData, c, cSet);
+        const auto &antiNodes = findAntiNodesP1(inputData, c, cSet);
         result.insert(antiNodes.begin(), antiNodes.end());
     }
 
+    return static_cast<int>(result.size());
+}
+
+
+std::set<Coord>
+findAntiNodesP2(const std::vector<std::string> &inputData, const char c, const std::set<Coord> &coords) {
+    std::set<Coord> result;
+
+    // pair the antennas
+    auto cSize = static_cast<int>(coords.size());
+
+    for (int i = 0; i < cSize; i++) {
+        for (int j = i + 1; j < cSize; j++) {
+            const auto &[cAX, cAY] = *std::next(coords.begin(), i);
+            const auto &[cBX, cBY] = *std::next(coords.begin(), j);
+
+
+            int dX1 = cAX - cBX, dY1 = cAY - cBY;
+            auto aX = cAX + dX1, aY = cAY + dY1;
+            while (isInMap(inputData, {aX, aY})) {
+                if (inputData[aX][aY] != c) {
+                    result.insert({aX, aY});
+                }
+                aX += dX1;
+                aY += dY1;
+            }
+
+            int dX2 = cBX - cAX, dY2 = cBY - cAY;
+            auto bX = cBX + dX2, bY = cBY + dY2;
+            while (isInMap(inputData, {bX, bY})) {
+                if (inputData[bX][bY] != c) {
+                    result.insert({bX, bY});
+                }
+                bX += dX2;
+                bY += dY2;
+            }
+        }
+    }
+
+    return result;
+}
+
+int solverP2(const std::vector<std::string> &inputData) {
+    std::set<Coord> result;
+
+    const auto inputMap = preprocessor(inputData);
+
+    for (const auto &[c, cSet]: inputMap) {
+        const auto &antiNodes = findAntiNodesP2(inputData, c, cSet);
+        result.insert(cSet.begin(), cSet.end());
+        result.insert(antiNodes.begin(), antiNodes.end());
+    }
 
     return static_cast<int>(result.size());
 
@@ -139,8 +184,8 @@ int main(int argc, char *argv[]) {
     auto resultP1 = solverP1(input);
     std::cout << "P1 result: " << resultP1 << std::endl;
 
-//    int resultP2 = solverP2(sList);
-//    std::cout << "P2 result: " << resultP2 << std::endl;
+    auto resultP2 = solverP2(input);
+    std::cout << "P2 result: " << resultP2 << std::endl;
 
     return 0;
 }
